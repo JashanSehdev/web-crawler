e06e84af887d2c161c685d6b963c9daf493e33f7
Jashan Sehdev <sehdevjashan321@gmail.com>
2026-04-18 11:47:40 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,46 +1,69 @@
-const { JSDOM } = require('jsdom')
+const { JSDOM } = require("jsdom");
 
-function getURLsfromHTML(htmlBody, baseURL){
-const urls = [];
-const dom = new JSDOM(htmlBody);
-// console.log(dom);
-const linkElements = dom.window.document.querySelectorAll('a');
-for (const linkElement of linkElements) {
+async function crawlPage(currentURL) {
+    console.log("activily crawling: ", currentURL);
+    try{
+      const resp = await fetch(currentURL);
+      if (resp.status > 399) {
+        console.log(`Error in fetch status code ${resp.status} on site: ${currentURL}`);
+        return;
+      }
+
+      const contentType = resp.headers.get("content-type")
+      if (!contentType.includes("text/html")) {
+        console.log(`non html response, content type: ${contentType} on page ${currentURL}`);
+        return;
+      }
+
+      console.log(await resp.text());
+      
+      
+    } catch (err) {
+      console.log(`Error in fetch :${err.message}, on page: ${currentURL}`);
+      
+    }
+    
+}
+
+function getURLsfromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  // console.log(dom);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
     console.log(linkElement.href);
-    if (linkElement.href.slice(0,1) === '/'){
-        //relative
-        try{
-            const urlObj = new URL(`${baseURL}${linkElement.href}`)
-            urls.push(urlObj.href);
-        } catch (err) {
-            console.log("error with relative URL:", err.message);
-        }
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("error with relative URL:", err.message);
+      }
     } else {
-        try{
-            const urlObj = new URL(linkElement.href)
-            urls.push(urlObj.href);
-        } catch (err) {
-            console.log("error with absolute url", err.message);
-        }
-        
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("error with absolute url", err.message);
+      }
     }
-
-}
-return urls;
+  }
+  return urls;
 }
 
-function normalizeUrl(urlString){
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === "/"){
-        return hostPath.slice(0, -1);
-    }
+function normalizeUrl(urlString) {
+  const urlObj = new URL(urlString);
+  const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
+  if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
+    return hostPath.slice(0, -1);
+  }
 
-    return hostPath;
+  return hostPath;
 }
 
 module.exports = {
-    normalizeUrl,
-    getURLsfromHTML
-    
-}
+  normalizeUrl,
+  getURLsfromHTML,
+  crawlPage
+};
---
+++ main.js
@@ -0,0 +1,21 @@
+const {crawlPage} = require('./crawl.js')
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("no website provided");
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log("too many command Line args");
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+    console.log(`start crawling ${baseURL}`);
+    
+    crawlPage(baseURL);
+
+    
+}
+
+main()
