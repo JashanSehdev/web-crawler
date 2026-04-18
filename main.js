f29856f03337d38acad27493b52110107f498f70
Jashan Sehdev <sehdevjashan321@gmail.com>
2026-04-18 13:01:49 +0530

    feature and tested: proper report print with sorted hits on pages

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const {crawlPage} = require('./crawl.js')
+const {crawlPage} = require('./crawl.js');
+const { printReport } = require('./report.js');
 
-function main() {
+async function main() {
     if (process.argv.length < 3) {
         console.log("no website provided");
         process.exit(1);
@@ -13,7 +14,10 @@ function main() {
     const baseURL = process.argv[2];
     console.log(`start crawling ${baseURL}`);
     
-    crawlPage(baseURL);
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+
+    printReport(pages);
 
     
 }
